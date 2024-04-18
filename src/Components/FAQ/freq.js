37b742c687fdec3ce0348f479c779683835f0bfb
lilyflowr <ibadiela@gmail.com>
2024-04-18 18:08:34 +0100

    FAQs, intsalled tailwind css lib

--- src/Components/FAQ/freq.js
+++ src/Components/FAQ/freq.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './freq.css'
+import './freq.css';
 
 function Question({ ques, answ }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,16 +9,18 @@ function Question({ ques, answ }) {
   };
 
   return (
-    <div className="ques" onClick={toggleAnsw}>
-      <div className="question-text">{ques}</div>
-      {isOpen && <div className="answ">{answ}</div>}
+    <div className={`p-5 border-b border-gray-300 mb-2 last:border-b-0 ${isOpen ? 'open-bg' : 'bg-white floating-shadow'}`}>
+      <div className="flex justify-between items-center cursor-pointer" onClick={toggleAnsw}>
+        <h2 className={`text-base md:text-lg lg:text-xl font-bold text-gray-800 leading-relaxed`}>{ques}</h2>
+        <span className={`text-xl transform transition-transform ${isOpen ? 'rotate-180' : ''}`}>▼</span>
+      </div>
+      {isOpen && <p className="mt-2 text-gray-600 text-sm md:text-base leading-relaxed">{answ}</p>}
     </div>
   );
 }
-
 function Freq() {
   return (
-    <div className="container">
+    <div className="max-w-5xl  mx-auto my-5 p-5 bg-gray-100 rounded-lg shadow-lg">
       <Question
         ques="What is HTML?"
         answ="HTML stands for HyperText Markup Language. It is the standard markup language for creating web pages and web applications."
@@ -42,5 +44,4 @@ function Freq() {
     </div>
   );
 }
-
 export default Freq;
