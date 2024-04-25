0c1680831c710f5dea4e337d6981486f7bea98f2
lilyflowr <ibadiela@gmail.com>
2024-04-25 07:42:23 +0100

    added Moromokes authfiles minor edits restrctd componentsFolder

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { MenuItems } from '../quiz/MenuItems';
+import './Navbar.css';
+import Logo from '../../Assets/Insightify Logo.png'; // Import your logo image
+
+class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked });
+    }
+
+    render() {
+        return (
+            <nav className='NavbarItems'>
+                <div className='navbar-logo'>
+                    <img src={Logo} alt="Logo" /> {/* Add your logo image */}
+                    <h2>...the best online career compass</h2> {/* Add the heading */}
+                </div>
+                <div className='menu-icon' onClick={this.handleClick}>
+                    <i className={this.state.clicked ? "fa fa-times" : "fa fa-bars"}></i>
+                </div>
+                <ul className={this.state.clicked ? "nav-menu active" : "nav-menu"}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default Navbar;
