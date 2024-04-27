37611f35ce8c783dbad7b5b868980299ee740d60
moromoke01 <bellomoromoke2019@gmail.com>
2024-04-27 17:43:43 +0100

    backend deployed and integrated to work on cloud

--- src/Components/LoginSignup/Login.js
+++ src/Components/LoginSignup/Login.js
@@ -22,7 +22,7 @@ const Login = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('https://insignify-backend.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
