fd48f0af84ef9015c985efd65ab41ed888cfd642
lilyflowr <ibadiela@gmail.com>
2024-04-28 22:52:47 +0100

    newsletter to formspree and responsive

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,10 +8,16 @@ import foot_three from '../../Assets/img3.jpeg';
 import Freq from '../FAQ/freq';
 import Footer from '../Footer/Footer';
 import Navbar from '../Navbar/Navbar';
+import { useForm, ValidationError } from '@formspree/react';
+
 
 const Home = () => {
-  // const progress = 80;
-
+  const [state, handleSubmit] = useForm("mjvnlvdd");
+  if (state.succeeded) {
+    console.log("success!");
+        document.getElementById("contact-form").reset();
+}
+  // const progress = 80; bikonu whats this cpode for?
   // function getRandomPosition() {
   //   const maxX = window.innerWidth - 100;
   //   const maxY = window.innerHeight - 100;
@@ -159,18 +165,76 @@ const Home = () => {
           ))}
         </div>
       </div>
+      
       {/* newsletter  */}
-  <div className="newsletter-section">
-  <h1>Subscribe to Our Newsletter</h1>
-  <div className="form-container">
-    <div className="email-input ">
-      <label htmlFor="email" className="email-label">Email Address</label>
-      <input type="email" id="email" name="email" placeholder="Enter your email address" className="email-input-box" />
-    </div>
+      <aside
+    class="newsletter-section p-4 my-8 bg-white border  rounded-lg shadow-md sm:p-6 lg:p-8 "
+    >
+      
+      <h1 class="mb-3 text-xl font-medium text-gray-900 dark:text-white">Subscribe to Our Newsletter</h1>
 
-    <button className="subscribe-button">SUBSCRIBE</button>
+      <form id="contact-form" onSubmit={handleSubmit}>
+  <div data-style="clean" class="flex items-center mb-3">
+    <div data-element="fields" data-stacked="false"
+      class="flex items-center w-full seva-fields formkit-fields">
+      <div class="relative w-full mr-3 formkit-field">
+        
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter Your Email"
+          class="formkit-input bg-transparent border-none border-b-2 border-purple-500 text-gray-900 text-md focus:ring-0 focus:border-purple-500 block w-full pl-3 p-2.5  dark:text-white"
+        />
+        <ValidationError 
+          prefix="Email" 
+          field="email"
+          errors={state.errors}
+        />
+      </div>
+      <button type='submit' disabled={state.submitting} className="px-5 py-3 text-sm font-medium text-center text-white bg-purple-700 rounded-lg cursor-pointer hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 ml-0">SUBSCRIBE</button>
+    </div>
   </div>
-</div>
+</form>
+</aside>
+
+{/* <aside
+    class="newsletter-section p-4 my-8 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700"
+    >
+   
+    <form>
+        <div data-style="clean" class="flex items-end mb-3">
+            <div data-element="fields" data-stacked="false"
+                class="flex items-center w-full max-w-md mb-3 seva-fields formkit-fields">
+                <div class="relative w-full mr-3 formkit-field">
+                    <label for="member_email" 
+                    class="hidden block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                      Email address</label>
+                    <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path>
+                            <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path>
+                        </svg>
+                    </div>
+                    <input id="member_email"
+                     class=" formkit-input bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      name="email_address" aria-label="Email Address" 
+                      placeholder="Your email address..." required="" type="email" />
+                </div>
+                <span
+                 class="px-5 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg cursor-pointer hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Subscribe</span>
+            </div>
+        </div>
+    </form>
+
+    <div class="text-sm font-medium text-gray-500 dark:text-gray-300">By subscribing, you agree with ConvertKit's <a
+            href="#" class="text-blue-600 hover:underline dark:text-blue-500">Terms of Service</a> and
+        <a class="text-blue-600 hover:underline dark:text-blue-500" href="#">Privacy
+            Policy
+        </a>.
+    </div>
+</aside> */}
   <Footer />
 </>
   );
