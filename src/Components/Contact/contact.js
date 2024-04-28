89cbdb192e8e4b75ce26120299c4ad54baa83db4
Mojisola-Akinrelere <mojisolaoakinrelere@gmail.com>
2024-04-28 16:51:52 +0100

    updated contact page

--- src/Components/Contact/contact.js
+++ src/Components/Contact/contact.js
@@ -12,6 +12,8 @@ const ContactUsPage = () => {
     message: ''
   });
 
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData({
@@ -31,6 +33,8 @@ const ContactUsPage = () => {
       subject: '',
       message: ''
     });
+    // Set submission status to true
+    setIsSubmitted(true);
   };
 
   return (
@@ -38,53 +42,61 @@ const ContactUsPage = () => {
       <Navbar />
       <div className="contact-us-container">
         <div className="contact-info">
-          <h4>Get in touch with us</h4>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="subject">Subject:</label>
-              <input
-                type="text"
-                id="subject"
-                name="subject"
-                value={formData.subject}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Message:</label>
-              <textarea
-                id="message"
-                name="message"
-                value={formData.message}
-                onChange={handleChange}
-                required
-              ></textarea>
-            </div>
-            <button type="submit">Send Message</button>
-          </form>
+          <h4>Get in Touch with us</h4>
+          {isSubmitted ? (
+            <p>Your message was sent successfully. We will respond to you shortly.</p>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Enter your name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="subject">Subject</label>
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  placeholder="Enter your subject"
+                  value={formData.subject}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  id="message"
+                  name="message"
+                  placeholder="Enter your message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  required
+                ></textarea>
+              </div>
+              <button type="submit">SEND A MESSAGE</button>
+            </form>
+          )}
         </div>
         <div className="contact-image">
           <img src={ContactImg} alt="Contact Us" />
